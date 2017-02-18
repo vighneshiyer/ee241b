7158a3268a303acb3853435924bbdf0ce37eb531
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-02-17 16:30:08 -0800

    working on problem 2, problem 1 and 3 are done

--- hw1/delay_modeling/delay_modeling.m
+++ hw1/delay_modeling/delay_modeling.m
@@ -18,11 +18,52 @@ fclose(inverter_delay_file);
 pmos_width = str2double(inverter_delay_data(:,1)) / 1e-9;
 low_to_high_delay = str2double(inverter_delay_data(:,2)) / 1e-12;
 high_to_low_delay = str2double(inverter_delay_data(:,3)) / 1e-12;
+avg_prop_delay = (low_to_high_delay + high_to_low_delay)/2;
 plot(pmos_width, low_to_high_delay);
 hold on
 plot(pmos_width, high_to_low_delay);
-plot(pmos_width, (low_to_high_delay + high_to_low_delay)/2);
+plot(pmos_width, avg_prop_delay);
 xlabel('PMOS Width (nm)', 'Interpreter', 'Latex');
 ylabel('Delay (ps)', 'Interpreter', 'Latex');
 legend('Low-to-High Delay', 'High-to-Low Delay', 'Avg Propagation Delay');
+[min_avg_prop_delay, i] = min(avg_prop_delay);
+min_avg_prop_delay_pmos_width = pmos_width(i);
 title('Propagation Delay of an Inverter Calculated using F04 Method')
+
+%% plotting delay vs fanout for optimally sized inverter
+delay_fanout_file = fopen('delay_fanout.txt');
+% file is in format PMOS width (meters), low to high delay, high to low
+% delay, temperature
+
+delay_fanout_data = [];
+tline = fgets(delay_fanout_file);
+while ischar(tline)
+    line_split = strsplit(tline);
+    line_size = size(line_split);
+    if (line_size(2) == 6)
+        delay_fanout_data = cat(1, delay_fanout_data, line_split(2:4));
+    end
+    tline = fgets(delay_fanout_file);
+end
+fclose(delay_fanout_file);
+
+fanout = str2double(delay_fanout_data(2:114,1));
+low_to_high_delay = str2double(delay_fanout_data(2:114,2)) / 1e-12;
+high_to_low_delay = str2double(delay_fanout_data(2:114,3)) / 1e-12;
+avg_prop_delay = (low_to_high_delay + high_to_low_delay)/2;
+plot(fanout, low_to_high_delay);
+hold on
+plot(fanout, high_to_low_delay);
+plot(fanout, avg_prop_delay);
+xlabel('Fanout', 'Interpreter', 'Latex');
+ylabel('Delay (ps)', 'Interpreter', 'Latex');
+title('Propagation Delay of an Optimally Sized Inverter with Increasing Fanout')
+
+% now let's fit a curve
+% params(1) = t_inv
+% params(2) = gamma
+% t_p = t_inv(gamma + f) where f is fanout
+prop_delay_fn = @(params, data)params(1) .* (params(2) + data);
+inverter_intrinsic_params = lsqcurvefit(prop_delay_fn, [10, 1.1], fanout, avg_prop_delay);
+plot(fanout, prop_delay_fn(inverter_intrinsic_params, fanout));
+legend('Low-to-High Delay', 'High-to-Low Delay', 'Avg Propagation Delay', 'Prop Delay Fn Fit');
