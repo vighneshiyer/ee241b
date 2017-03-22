72057532acf0762826ca35b58a6f96f91530d0e6
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-03-21 23:37:46 -0700

    done with problem 1, almost done with 2

--- hw3/inverter_variation/inverter_variation.m
+++ hw3/inverter_variation/inverter_variation.m
@@ -166,89 +166,29 @@ xlabel('$V_{DD}$ (V)', 'Interpreter', 'Latex');
 ylabel('Delay (ps)', 'Interpreter', 'Latex');
 legend({'6$\sigma$ delay', 'average delay'}, 'Interpreter', 'Latex');
 
-%% problem 3a preface, plotting inverter delay as a function of PMOS width
-inverter_delay_file = fopen('part_a/inverter_delay_pmos_sweep.txt');
-% file is in format PMOS width (meters), low to high delay, high to low
-% delay, temperature
+%% problem 1d monte carlo matlab sim
+syms VDD;
+syms delta_Vth;
+C = 1.5e-15;
+K = 1.7e-4;
+V_th = 0.336;
+alpha = 1.36;
+sigma_Vth = 16.01e-3;
+alpha_power_delay = (C * 0.5 * VDD) / (K * (VDD - V_th + delta_Vth)^alpha);
 
-inverter_delay_data = [];
-tline = fgets(inverter_delay_file);
-while ischar(tline)
-    line_split = strsplit(tline);
-    line_size = size(line_split);
-    if (line_size(2) == 6)
-        inverter_delay_data = cat(1, inverter_delay_data, line_split(2:4));
-    end
-    tline = fgets(inverter_delay_file);
+Vth_variations = [];
+for i = 1:600
+    Vth_variations = cat(1, Vth_variations, normrnd(0, sigma_Vth));
 end
-fclose(inverter_delay_file);
 
-pmos_width = str2double(inverter_delay_data(:,1)) / 1e-9;
-low_to_high_delay = str2double(inverter_delay_data(:,2)) / 1e-12;
-high_to_low_delay = str2double(inverter_delay_data(:,3)) / 1e-12;
-avg_prop_delay = (low_to_high_delay + high_to_low_delay)/2;
-plot(pmos_width, low_to_high_delay);
-hold on
-plot(pmos_width, high_to_low_delay);
-plot(pmos_width, avg_prop_delay);
-xlabel('PMOS Width (nm)', 'Interpreter', 'Latex');
-ylabel('Delay (ps)', 'Interpreter', 'Latex');
-legend('Low-to-High Delay', 'High-to-Low Delay', 'Avg Propagation Delay');
-[min_avg_prop_delay, i] = min(avg_prop_delay);
-min_avg_prop_delay_pmos_width = pmos_width(i);
-title('Propagation Delay of an Inverter Calculated using F04 Method')
+delays_105 = subs(alpha_power_delay, VDD, 1.05);
+delays_105 = double(subs(delays_105, delta_Vth, Vth_variations));
 
-%% problem 3b, monte carlo
-monte_carlo_file = fopen('part_b/monte_carlo_data.txt');
-
-monte_carlo_data = [];
-tline = fgets(monte_carlo_file);
-while ischar(tline)
-    line_split = strsplit(tline);
-    line_size = size(line_split);
-    if (line_size(2) == 5)
-        monte_carlo_data = cat(1, monte_carlo_data, line_split(2:4));
-    end
-    tline = fgets(monte_carlo_file);
-end
-fclose(monte_carlo_file);
-monte_carlo_hl_delay = str2double(monte_carlo_data(:,2)) / 1e-12;
-monte_carlo_lh_delay = str2double(monte_carlo_data(:,3)) / 1e-12;
-subplot(1, 2, 1);
-histfit(monte_carlo_lh_delay);
+delays_06 = subs(alpha_power_delay, VDD, 0.6);
+delays_06 = double(subs(delays_06, delta_Vth, Vth_variations));
+histfit(delays_105 / 1e-12);
+hold on;
+histfit(delays_06 / 1e-12);
+title('Alpha-Power Law Based Delay w/ MATLAB Monte Carlo', 'Interpreter', 'Latex');
 xlabel('Delay (ps)', 'Interpreter', 'Latex');
 ylabel('Frequency', 'Interpreter', 'Latex');
-title('Low-to-High Delay of Optimal Inverter');
-subplot(1, 2, 2);
-histfit(monte_carlo_hl_delay);
-xlabel('Delay (ps)', 'Interpreter', 'Latex');
-ylabel('Frequency', 'Interpreter', 'Latex');
-title('High-to-Low Delay of Optimal Inverter');
-
-%% problem 3c, temperature variation
-temp_file = fopen('part_c/temp_data.txt');
-
-temp_data = [];
-tline = fgets(temp_file);
-while ischar(tline)
-    line_split = strsplit(tline);
-    line_size = size(line_split);
-    if (line_size(2) == 6)
-        temp_data = cat(1, temp_data, line_split(2:4));
-    end
-    tline = fgets(temp_file);
-end
-fclose(temp_file);
-temp = str2double(temp_data(:,1));
-temp_lh_delay = str2double(temp_data(:,2)) / 1e-12;
-temp_hl_delay = str2double(temp_data(:,3)) / 1e-12;
-subplot(1, 2, 1);
-plot(temp, temp_lh_delay);
-xlabel('Temp (C)', 'Interpreter', 'Latex');
-ylabel('Delay (ps)', 'Interpreter', 'Latex');
-title('Low-to-High Delay of Optimal Inverter');
-subplot(1, 2, 2);
-plot(temp, temp_hl_delay);
-xlabel('Temp (C)', 'Interpreter', 'Latex');
-ylabel('Delay (ps)', 'Interpreter', 'Latex');
-title('High-to-Low Delay of Optimal Inverter');
